2160d34cd4b005aadc66f631a67e0d00f0c49d2a
Nilton <nduducbnu@gmail.com>
2020-05-15 21:02:02 -0300

    geradorArquivo

--- AlgoritmoAEstrela/src/GeradorCaminhos.java
+++ AlgoritmoAEstrela/src/GeradorCaminhos.java
@@ -1,8 +1,12 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Nilton E. Clasen, Gustavo K. Heinen e Mateus Kienen
@@ -10,14 +14,133 @@
 public class GeradorCaminhos {
 
     public static void main(String[] args) {
-        teste5x5();
-        teste6x6();
-        teste7x7();
-        testeSemSaida();
-        testeDestinoOrigemIgual();
+        lerArquivo();
+//        testes já prontos
+//        teste5x5();
+//        teste6x6();
+//        teste7x7();
+//        testeSemSaida();
+//        testeDestinoOrigemIgual();
     }
 
-    
+    public static void lerArquivo() {
+        try {
+            String path = GeradorCaminhos.class.getResource("entrada5x5.txt").getPath();
+            FileReader arq = new FileReader(path);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha;
+            Objeto[][] grade = null;
+            Objeto origem = null;
+            Objeto destino = null;
+            ArrayList<Objeto> array = new ArrayList();
+            while ((linha = lerArq.readLine()) != null) {
+                if (linha.contains("tamanho")) {
+                    String[] aux = linha.split(":");
+                    int tamanho = Integer.parseInt(aux[1]);
+                    grade = new Objeto[tamanho][tamanho];
+                    for (int i = 0; i < grade.length; i++) {
+                        for (int j = 0; j < grade[i].length; j++) {
+                            grade[i][j] = new Objeto(i, j);
+                        }
+                    }
+                } else if (linha.contains("origem")) {
+                    linha = linha.substring(7);
+                    String[] aux = linha.split("-");
+                    int x = Integer.parseInt(aux[0]);
+                    int y = Integer.parseInt(aux[1]);
+                    origem = grade[x][y];
+                } else if (linha.contains("destino")) {
+                    linha = linha.substring(8);
+                    String[] aux = linha.split("-");
+                    int x = Integer.parseInt(aux[0]);
+                    int y = Integer.parseInt(aux[1]);
+                    destino = grade[x][y];
+                } else if (linha.contains("bloqueio")) {
+                    linha = linha.substring(9);
+                    String[] aux = linha.split("-");
+                    int x = Integer.parseInt(aux[0]);
+                    int y = Integer.parseInt(aux[1]);
+                    array.add(grade[x][y]);
+                }
+            }
+            AlgoritmoEstrela aStar = new AlgoritmoEstrela(grade, origem, destino);
+            for (Objeto obj : array) {
+                aStar.addBloqueio(obj);
+            }
+            boolean resultado = aStar.iniciarPesquisa();
+            imprimirDesafio(origem, destino, aStar, grade);
+            imprimirResultado(origem, destino, aStar, grade);
+            if (!resultado) {
+                System.out.println("\nNão foi possível encontrar o destino");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+    }
+
+    public static void imprimirResultado(Objeto origem, Objeto destino, AlgoritmoEstrela aStar, Objeto[][] grade) {
+        System.out.println("\n\n*********Desafio concluido*********");
+        for (int i = 0; i < grade.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < grade[i].length; j++) {
+                if (origem.getX() == j && origem.getY() == i) {
+                    System.out.print("[ O ]");
+                } else if (destino.getX() == j && destino.getY() == i) {
+                    System.out.print("[ D ]");
+                } else {
+                    boolean isCaminho = false;
+                    for (int k = 0; k < aStar.getListaCaminho().size(); k++) {
+                        if (aStar.getListaCaminho().get(k).getX() == j && aStar.getListaCaminho().get(k).getY() == i) {
+                            System.out.print("[ x ]");
+                            isCaminho = true;
+                            break;
+                        }
+                    }
+                    boolean isBloqueio = false;
+                    for (int k = 0; k < aStar.getListaBloqueios().size(); k++) {
+                        if (aStar.getListaBloqueios().get(k).getX() == j && aStar.getListaBloqueios().get(k).getY() == i) {
+                            System.out.print("[ • ]");
+                            isBloqueio = true;
+                            break;
+                        }
+                    }
+                    if (!isCaminho && !isBloqueio) {
+                        System.out.print("[   ]");
+                    }
+                }
+
+            }
+        }
+    }
+
+    public static void imprimirDesafio(Objeto origem, Objeto destino, AlgoritmoEstrela aStar, Objeto[][] grade) {
+        System.out.println("\n*********Desafio gerado*********");
+        for (int i = 0; i < grade.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < grade[i].length; j++) {
+                if (origem.getX() == j && origem.getY() == i) {
+                    System.out.print("[ O ]");
+                } else if (destino.getX() == j && destino.getY() == i) {
+                    System.out.print("[ D ]");
+                } else {
+                    boolean isBloqueio = false;
+                    for (int k = 0; k < aStar.getListaBloqueios().size(); k++) {
+                        if (aStar.getListaBloqueios().get(k).getX() == j && aStar.getListaBloqueios().get(k).getY() == i) {
+                            System.out.print("[ • ]");
+                            isBloqueio = true;
+                            break;
+                        }
+                    }
+                    if (!isBloqueio) {
+                        System.out.print("[   ]");
+                    }
+                }
+
+            }
+        }
+    }
+
     public static void testeDestinoOrigemIgual() {
         Objeto[][] grade = new Objeto[5][5];
         for (int i = 0; i < grade.length; i++) {
@@ -30,7 +153,7 @@ public class GeradorCaminhos {
         Objeto destino = grade[0][0];
         /*configura bloqueios, exemplo
         *
-        [ O ][   ][ • ][   ][ D ]
+        [ OD][   ][ • ][   ][   ]
         [   ][   ][ • ][   ][   ]
         [   ][   ][ • ][   ][   ]
         [   ][   ][ • ][   ][   ]
@@ -51,7 +174,7 @@ public class GeradorCaminhos {
             System.out.println("\nNão foi possível encontrar o destino");
         }
     }
-    
+
     public static void testeSemSaida() {
         Objeto[][] grade = new Objeto[5][5];
         for (int i = 0; i < grade.length; i++) {
@@ -197,68 +320,6 @@ public class GeradorCaminhos {
         imprimirResultado(origem, destino, aStar, grade);
         if (!resultado) {
             System.out.println("\nNão foi possível encontrar o destino");
-        }
-    }
-
-    public static void imprimirResultado(Objeto origem, Objeto destino, AlgoritmoEstrela aStar, Objeto[][] grade) {
-        System.out.println("\n\n*********Desafio concluido*********");
-        for (int i = 0; i < grade.length; i++) {
-            System.out.println("");
-            for (int j = 0; j < grade[i].length; j++) {
-                if (origem.getX() == j && origem.getY() == i) {
-                    System.out.print("[ O ]");
-                } else if (destino.getX() == j && destino.getY() == i) {
-                    System.out.print("[ D ]");
-                } else {
-                    boolean isCaminho = false;
-                    for (int k = 0; k < aStar.getListaCaminho().size(); k++) {
-                        if (aStar.getListaCaminho().get(k).getX() == j && aStar.getListaCaminho().get(k).getY() == i) {
-                            System.out.print("[ x ]");
-                            isCaminho = true;
-                            break;
-                        }
-                    }
-                    boolean isBloqueio = false;
-                    for (int k = 0; k < aStar.getListaBloqueios().size(); k++) {
-                        if (aStar.getListaBloqueios().get(k).getX() == j && aStar.getListaBloqueios().get(k).getY() == i) {
-                            System.out.print("[ • ]");
-                            isBloqueio = true;
-                            break;
-                        }
-                    }
-                    if (!isCaminho && !isBloqueio) {
-                        System.out.print("[   ]");
-                    }
-                }
-
-            }
-        }
-    }
-
-    public static void imprimirDesafio(Objeto origem, Objeto destino, AlgoritmoEstrela aStar, Objeto[][] grade) {
-        System.out.println("\n*********Desafio gerado*********");
-        for (int i = 0; i < grade.length; i++) {
-            System.out.println("");
-            for (int j = 0; j < grade[i].length; j++) {
-                if (origem.getX() == j && origem.getY() == i) {
-                    System.out.print("[ O ]");
-                } else if (destino.getX() == j && destino.getY() == i) {
-                    System.out.print("[ D ]");
-                } else {
-                    boolean isBloqueio = false;
-                    for (int k = 0; k < aStar.getListaBloqueios().size(); k++) {
-                        if (aStar.getListaBloqueios().get(k).getX() == j && aStar.getListaBloqueios().get(k).getY() == i) {
-                            System.out.print("[ • ]");
-                            isBloqueio = true;
-                            break;
-                        }
-                    }
-                    if (!isBloqueio) {
-                        System.out.print("[   ]");
-                    }
-                }
-
-            }
         }
     }
 }
